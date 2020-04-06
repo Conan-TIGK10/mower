93451f24ca901b4d5ce84dcca218b455c22e66fb
LucasArvid <lucas.arvidsson@hotmail.com>
2020-04-06 14:14:48 +0200

    redid implementation of bt handler, now accepts protocol and m,ultiple lines of data. Also wrote function to fill table with read data

--- main/BluetoothController.ino
+++ main/BluetoothController.ino
@@ -2,12 +2,13 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
+#include <MeBluetooth.h>
 
 /*
 Blue module can only be connected to port 3, 4, 5, 6 of base shield.
 */
-MeBluetooth bluetooth(PORT_4);
-
+MeBluetooth bluetooth(PORT_3);
+unsigned char table[128] = {0};
 void setup()
 {
     Serial.begin(9600);
@@ -15,17 +16,49 @@ void setup()
     Serial.println("Bluetooth Start!");
 }
 
-void btSendData(double[] outData) {
+void btEnd() {
+ bluetooth.end(); 
+}
+
+// needs to be called mutiple times for sending multiple data values
+void btSendPosData(int protocol, double outData) {
   if(bluetooth.available()) {
+    bluetooth.write(protocol);
+    bluetooth.write(",");
     bluetooth.write(outData);
+    bluetooth.write(",");
+    delay(20); // Might work without delay, need to ensure that its not called constantly
   }
 }
 
-double btReadData() {
- if(bluetooth.available()) {
-  return = bluetooth.read()
- } 
+//sending multiple data, needs size of array/pointer.
+void btSendMultiplePosData(int protocol, double* outData, int size) {
+  if(bluetooth.available()) {
+    bluetooth.write(protocol);
+    bluetooth.write(",");
+    for (int i = 0; i < size; i++){
+      bluetooth.write(outData[i]);
+      bluetooth.write(",");
+    }
+    delay(20); // Might work without delay, need to ensure that its not called constantly
+  }
 }
-  
+
+// Returns unsigne char table of bt data
+unsigned char btReadData() {
+ int readdata = 0,i = 0,count = 0;
+ if(bluetooth.available()) {
+   while((readdata = bluetooth.read()) != (int)-1) {
+      table[count] = readdata;
+      count++;
+      delay(1);
+    }
+    for(i = 0;i<count;i++) {
+      Serial.write(table[i]);
+    }
+ } 
+ return = table;
+}
+
 
 
--- main/main.ino
+++ main/main.ino
@@ -1,187 +1,86 @@
-#include <Me4Button.h>
-#include <Me7SegmentDisplay.h>
-#include <MeAuriga.h>
-#include <MeBaseBoard.h>
-#include <MeBluetooth.h>
-#include <MeBuzzer.h>
-#include <MeColorSensor.h>
-#include <MeCompass.h>
-#include <MeConfig.h>
-#include <MeDCMotor.h>
-#include <MeEEPROM.h>
-#include <MeEncoderMotor.h>
-#include <MeEncoderNew.h>
-#include <MeEncoderOnBoard.h>
-#include <MeFlameSensor.h>
-#include <MeGasSensor.h>
-#include <MeGyro.h>
-#include <MeHostParser.h>
-#include <MeHumitureSensor.h>
-#include <MeInfraredReceiver.h>
-#include <MeIR.h>
-#include <MeJoystick.h>
-#include <MeLEDMatrix.h>
-#include <MeLEDMatrixData.h>
-#include <MeLightSensor.h>
-#include <MeLimitSwitch.h>
-#include <MeLineFollower.h>
-#include <MeMbotDCMotor.h>
-#include <MeMCore.h>
-#include <MeMegaPi.h>
-#include <MeMegaPiDCMotor.h>
-#include <MeMegaPiPro.h>
-#include <MeMegaPiPro4DcMotor.h>
-#include <MeMegaPiProESCMotor.h>
-#include <MeOnBoardTemp.h>
-#include <MeOneWire.h>
-#include <MeOrion.h>
-#include <MePIRMotionSensor.h>
-#include <MePm25Sensor.h>
-#include <MePort.h>
-#include <MePotentiometer.h>
-#include <MePressureSensor.h>
-#include <MePS2.h>
-#include <MeRGBLed.h>
-#include <MeSerial.h>
-#include <MeShield.h>
-#include <MeShutter.h>
-#include <MeSmartServo.h>
-#include <MeSoundSensor.h>
-#include <MeStepper.h>
-#include <MeStepperOnBoard.h>
-#include <MeTemperature.h>
-#include <MeTouchSensor.h>
-#include <MeUltrasonicSensor.h>
-#include <MeUSBHost.h>
-#include <MeVoice.h>
-#include <MeWifi.h>
-
-/*  --Project Modules --
-   -- Auto compiled --
-   BluetoothController
-*/
-
 /*************************************************************************
 Makeblock bluetooth car controller test code, shoulde be replaced with project code.
-
 I included all makeblock library files, which should be available in our project repo.
 Library files should be placed into Arduino installation folder -> installation folder\Arduino\libraries\Makeblock\
 **************************************************************************/
-#include "Makeblock.h"
-#include <SoftwareSerial.h>
+#include <Arduino.h>
+#include <avr/wdt.h>
+#include <MeAuriga.h>
+#include "MeEEPROM.h"
 #include <Wire.h>
+#include "LineTrackerController.h"
 
-MeDCMotor MotorL(M1);  
-MeDCMotor MotorR(M2);
-MeBluetooth serial(PORT_4);
-MeUltrasonicSensor UltrasonicSensor(PORT_3);
-int moveSpeed = 190;
-int turnSpeed = 200;
-int distance=0;
-int randnum = 0;
-boolean leftflag,rightflag;
-int minSpeed = 48;
-int factor = 23;
+typedef enum {STOP, FORWARD, BACKWARDS, RANDOM, ROTATE_RIGHT, ROTATE_LEFT} States;
+
+States state = STOP;
+#include <SoftwareSerial.h>
+
 
 void setup()
 {
-    leftflag=false;
-    rightflag=false;
-    serial.begin(115200);
-    BluetoothController.setup()
+  Serial.begin(9600);
+  gyroSetup();
+
 }
-int s1=0;
-int s2=0;
-int s3=0;
 
 void loop()
 {
-  if(serial.paramAvailable()){
-    s1 = serial.getParamValue("s1");
-    s2 = serial.getParamValue("s2");
-    if(serial.getParamValue("s3")>0){
-      s3 = serial.getParamValue("s3");
-    }
-    MotorL.run(s1);
-    MotorR.run(s2);
-    return;
-  }
-   if(s3==2&&millis()%50 == 0)
-   {
-     Serial.println("ultrasonic");
-      ultrCarProcess();
-   }
+  //LT_IsInside();
+  //delay(500);
+  // ChangeDirectionRandom();
+  int temp = LT_IsInside();
+  state_machine(temp);
+  delay(50);
 }
 
-void Forward()
+void state_machine(int16_t sensors) 
 {
-  MotorL.run(moveSpeed);
-  MotorR.run(moveSpeed);
-}
-void Backward()
-{
-  MotorL.run(-moveSpeed);
-  MotorR.run(-moveSpeed);
-}
+  Serial.print("state: ");
+  Serial.println(state);
+  Serial.println(sensors);
+    
+  switch(state)
+  {
+    case STOP:
+      if (sensors == BOTH) {
+        Stop();
+      }
+        else if(sensors == NONE) {
+        state = FORWARD;
+      } else if (sensors == SENSOR_RIGHT) {
+        //rotate left
+        state = ROTATE_LEFT;
+      } else if (sensors == SENSOR_LEFT){
+        //rotate right
+        state = ROTATE_RIGHT;
+      }
+      break;
+      
+    case FORWARD:
+      if (sensors != NONE) {
+        state = STOP;
+      } else {
+        Forward();
+      }
+      break;
+      
+    case ROTATE_LEFT:
+      if (sensors != SENSOR_RIGHT) {
+        state = STOP;
+      } else {
+        StopBackwardAndTurnRight();
+      }
+       
+      break;
 
-void BackwardAndTurnLeft()
-{
-  MotorL.run(-moveSpeed/2);
-  MotorR.run(-moveSpeed);
-}
-void BackwardAndTurnRight()
-{
-  MotorL.run(-moveSpeed);
-  MotorR.run(-moveSpeed/2);
-}
-void TurnLeft()
-{
-  MotorL.run(-moveSpeed);
-  MotorR.run(moveSpeed);
-}
-void TurnRight()
-{
-  MotorL.run(moveSpeed);
-  MotorR.run(-moveSpeed);
-}
-void Stop()
-{
-  MotorL.run(0);
-  MotorR.run(0);
-}
-void ChangeSpeed(int spd)
-{
-  moveSpeed = spd;
-}
-
-void ultrCarProcess()
-{
-  distance = UltrasonicSensor.distanceCm();
-  randomSeed(analogRead(A4));
-  if(distance>10&&distance<40)
-  {
-    randnum=random(300);
-    if(randnum > 190 && !rightflag)
-    {
-      leftflag=true;
-      TurnLeft();   
-    }
-    else
-    {
-      rightflag=true;
-      TurnRight();  
-    }
-  }
-  else if(distance<10)
-  {
-     randnum=random(300);
-    if(randnum > 190)BackwardAndTurnLeft();
-    else BackwardAndTurnRight();
-  }
-  else
-  {
-      leftflag=false;
-      rightflag=false;
-      Forward();
-  }
+    case ROTATE_RIGHT:
+      if (sensors != SENSOR_LEFT ){
+         state = STOP;
+      } else {
+        StopBackwardAndTurnLeft();
+      }
+      break;
+      
+  } 
+  runGyro();
 }
